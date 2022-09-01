1f04be9376ca426d92dc243ea264e200d61d14e1
irpansyah2m1d <irpansyah810@gmail.com>
2022-09-01 14:48:55 +0700

    first commit

---
+++ dist/myscript.js
@@ -0,0 +1,16 @@
+$(function () {
+  // swal({
+  //   title: "Profile Picture",
+  //   text: "Do you want to make the above image your profile picture?",
+  //   imageUrl: "alquran.jepg",
+  //   imageWidth: 550,
+  //   imageHeight: 225,
+  //   imageAlt: "Eagle Image",
+  //   showCancelButton: true,
+  //   confirmButtonText: "Yes",
+  //   cancelButtonText: "No",
+  //   confirmButtonColor: "#00ff55",
+  //   cancelButtonColor: "#999999",
+  //   reverseButtons: true,
+  // });
+});
